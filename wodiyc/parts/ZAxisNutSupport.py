5ae60b1657cc93f6267b77e6aa14e5759b04acca
Andreas Florath <andreas@florath.net>
2018-12-25 16:24:19 +0100

    Refactor ZAxis Nut Support
    Signed-off-by: Andreas Florath <andreas@florath.net>

--- wodiyc/parts/ZAxisNutSupport.py
+++ wodiyc/parts/ZAxisNutSupport.py
@@ -18,7 +18,10 @@ def measurements_ZAxisNutSupport(m):
                + 2 * m.ZAxisPlatform.cutout_depth \
                - m.ZAxisBearingSupport.z_size
     print("ZAxisNutSupport y_size [%.5f]" % p.y_size)
+    p.cutout_depth = m.Common.base_material_cutout_depth
     p.z_size = m.Common.base_material_thickness
+    p.z_size_real = m.Common.base_material_real_thickness
+    p.z_diff = p.z_size_real - p.z_size
 
 class ZAxisNutSupport:
 
@@ -42,14 +45,14 @@ class ZAxisNutSupport:
         for x in (x_center - x_diff, x_center + x_diff):
             for y in (y_center - y_diff, y_center + y_diff):
                 self.__gf_upper.cylinder(
-                    x, y, m.Common.screwhole_diameter, p.z_size)
+                    x, y, m.Common.screwhole_diameter, p.z_size_real)
                 self.__gf_upper.free_movement()
 
         for gf in (self.__gf_upper, self.__gf_lower):
             # Central hole
             gf.cylinder(x_center, y_center,
                         m.Common.threaded_rod_x_free_movement_diameter,
-                        p.z_size)
+                        p.z_size_real)
             gf.free_movement()
 
             # Crossnuts to fix the platform of the Z axis bearing support
@@ -59,11 +62,20 @@ class ZAxisNutSupport:
                 for y in (p.cross_nut_distance_from_edge,
                           p.y_size - p.cross_nut_distance_from_edge):
                     gf.cylinder(
-                        x, y, m.Common.cross_nut_diameter, p.z_size)
+                        x, y, m.Common.cross_nut_diameter, p.z_size_real)
                     gf.free_movement()
 
+            offset = gf.get_tool_diameter() / 2
+            gf.pocket(
+                0, 0, self.p.x_size, self.p.cutout_depth + offset, self.p.z_diff)
+            gf.free_movement()
+            gf.pocket(
+                0, self.p.y_size - self.p.cutout_depth - offset,
+                self.p.x_size, self.p.cutout_depth + offset, self.p.z_diff)
+            gf.free_movement()
+                    
             # Platform
-            gf.cutout_rect(0, 0, p.x_size, p.y_size, p.z_size)
+            gf.cutout_rect(0, 0, p.x_size, p.y_size, p.z_size_real)
             gf.free_movement()
 
             gf.comment("ZAxisNutSupport x_size [%.5f] y_size [%.5f]"
